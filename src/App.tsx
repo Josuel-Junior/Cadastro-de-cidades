730090b47dcdfe12145bfd24703ac7a9529bb5e5
Josuel Junior <108230374+Josuel-Junior@users.noreply.github.com>
2023-09-21 13:29:02 -0300

    feat: add theme light and dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./pages/routes";
+import { ThemeProvider } from "@emotion/react";
+import { LightTheme } from "./pages/shared/themes";
 
 export const App = () => {
   return (
+    <ThemeProvider theme={LightTheme}>
     <BrowserRouter>
       <AppRoutes/>
     </BrowserRouter>
+    </ThemeProvider>
   );
 };
